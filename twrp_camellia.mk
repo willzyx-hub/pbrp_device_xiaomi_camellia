ac8120bedad5d3baea912700ee845d8202edab98
willzyx-hub <86699027+willzyx-hub@users.noreply.github.com>
2021-11-27 20:09:04 +0700

    Update twrp_camellia.mk

--- twrp_camellia.mk
+++ twrp_camellia.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
 # Inherit some common recovery stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := camellia
